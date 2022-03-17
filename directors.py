8558116b8258a64ed72d7880e4c41e3de9e09466
Matthew Gilmore <mattsg@email.unc.edu>
2022-03-17 14:51:39 -0400

    update to directors scraping

--- directors.py
+++ directors.py
@@ -20,25 +20,25 @@ for f in films:
     except:
         print(f"no director at {url}")
 
-max_value: int = 0
 max_director: list = list()
+num_films_directed = directors.values()
+max_value = max(num_films_directed)
 
 for key in directors:
-    if(directors[key] >= max_value):
-        max_value = directors[key]
+    if(directors[key] == max_value):
         max_director.append(key)
 
 if(len(max_director) == 1):
-    print(f"\nYour most watched director is {max_director}!\n")
-    print(f"You have seen {directors[max_director]} films directed by {max_director}, which is {((max_value/total_films) * 100):.2f}% of the films you have logged.\n")
+    print(f"\nYour most watched director is {max_director[0]}!\n")
+    print(f"You have seen {directors[max_director[0]]} films directed by {max_director[0]}, which is {((max_value/total_films) * 100):.2f}% of the films you have logged.\n")
 else:
     print("\nYour most watched directors are: ")
     for d in max_director:
         print(d + ', ', end='')
     print("and... that's it!\n", end='')
-    print(f"\nYou have seen {max_value} films from each of these directors, which is {((max_value*len(max_director)/total_films)*100):.2f}% of the films you have logged.\n")
+    print(f"\nYou have seen {max_value} films from each of these directors, which makes up {((max_value*len(max_director)/total_films)*100):.2f}% of the films you have logged.\n")
 
-# Sean Fenessey
-# Your most watched directors are: 
-# Robert Aldrich, Jane Campion, Francis Ford Coppola, Wes Craven, Steven Soderbergh, Martin Scorsese,  and... that's it!
-# You have seen 17 films from each of these directors, which is 5.71% of the films you have logged.
+# User Martin_Blanke
+# Your most watched director is Alfred Hitchcock!
+
+# You have seen 19 films directed by Alfred Hitchcock, which is 3.80% of the films you have logged.
